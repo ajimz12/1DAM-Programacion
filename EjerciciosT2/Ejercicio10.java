3d73898b68b315c6fc09947b38d1a43dffd86f4f
Álvaro <114770098+ajimz12@users.noreply.github.com>
2022-11-08 16:11:56 +0100

    Add files via upload

---
+++ EjerciciosT2/Ejercicio10.java
@@ -0,0 +1,43 @@
+/*
+ */
+package ejercicio10t2;
+
+/**
+ *
+ * @author álvaro <alvarojimgil12@gmail.com>
+ */
+import bpc.daw.objetos.*;
+
+
+public class Ejercicio10t2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Caja c = new Caja ("Hola buenas");
+        
+        c.abrirCaja();
+        
+        String m = c.getMensaje();
+        
+        System.out.println(m);
+  
+        c.cambiarMensaje("holahola");
+        
+        String m2 = c.getMensaje();
+        
+        System.out.println(m2);
+        
+      
+        
+
+        
+        
+        
+        
+        
+    }
+    
+}
