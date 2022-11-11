b059fbf012259dc023f7c37e1caadcb272cf293c
Álvaro <114770098+ajimz12@users.noreply.github.com>
2022-11-11 17:01:59 +0100

    Add files via upload

---
+++ 02-Ejercicios-String/Ejercicio01String.java
@@ -0,0 +1,22 @@
+/*
+ */
+package aa;
+
+/**
+ *
+ * @author álvaro <alvarojimgil12@gmail.com>
+ */
+public class Aa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String cadena = "    Hola Mundo!  x";
+        
+        System.out.println(cadena.replaceAll("^\\s*",""));
+        
+    }
+    
+}
