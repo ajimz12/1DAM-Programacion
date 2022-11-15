9d3b0cd78591ba41fa4b8b1ff2eadb11479965ab
Álvaro <114770098+ajimz12@users.noreply.github.com>
2022-11-15 17:08:56 +0100

    Add files via upload

---
+++ EjerciciosT2/Ejercicio14.java
@@ -0,0 +1,29 @@
+/*
+ */
+package ejercicio14t2;
+
+/**
+ *
+ * @author álvaro <alvarojimgil12@gmail.com>
+ */
+
+import java.util.Scanner;
+        
+public class Ejercicio14t2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Escribe una frase: ");
+        String f = new Scanner(System.in).nextLine();
+        
+        System.out.println("La letra justo en mitad es: "+f.charAt(0)/2);
+        
+        
+        
+    }
+    
+}
---
+++ EjerciciosT2/Ejercicio23.java
@@ -0,0 +1,70 @@
+/*
+ */
+package ejercicio23;
+
+/**
+ *
+ * @author álvaro <alvarojimgil12@gmail.com>
+ */
+import bpc.daw.objetos.TarjetaCredito;
+
+import java.util.Scanner;
+
+public class Ejercicio23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        TarjetaCredito t = new TarjetaCredito(1000,2222);
+        
+        try{
+            
+        System.out.println("El saldo es: "+t.getSaldo(2222));
+            
+        }catch(Exception e){
+            
+            System.out.println("Error");
+                  
+        }
+        
+        t.ingresarDinero(100);
+        
+        try{
+            
+        System.out.println("El saldo es: "+t.getSaldo(2222));
+            
+        }catch(Exception e){
+            
+         System.out.println("Error");
+         
+        } 
+        
+        try{
+        
+            t.sacarDinero(2800,2222);
+           
+            
+        }catch(Exception e){
+            
+         System.out.println("Error");
+
+        }
+        
+        try{
+            
+        System.out.println("El saldo es: "+t.getSaldo(2222));
+            
+        }catch(Exception e){
+            
+            System.out.println("Error");
+                  
+        }
+       
+        
+       
+        
+    }
+    
+}
