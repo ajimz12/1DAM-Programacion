34336fcb88175cea42dbf432ce43d1820bb93b4b
Álvaro <114770098+ajimz12@users.noreply.github.com>
2022-12-02 16:39:36 +0100

    Add files via upload

---
+++ EjerciciosT2/Ejercicio42.java
@@ -0,0 +1,28 @@
+
+package ejercicio42;
+
+/**
+ *
+ * @author álvaro <alvarojimgil12@gmail.com>
+ */
+public class Ejercicio42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    int max = 65;
+    int min = 29;
+    int rango = max - min + 1;
+    
+    int random = (int)(Math.random()*rango)+min;
+    
+        System.out.println(random);
+    
+    
+        
+        
+    }
+
+}
