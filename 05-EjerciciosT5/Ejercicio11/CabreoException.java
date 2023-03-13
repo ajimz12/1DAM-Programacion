f29a014ff63683ed68679fe8c1e29d6399be4dd7
Álvaro <alvarojimgil12@gmail.com>
2023-03-13 21:07:40 +0100

    ADD ejercicio11

---
+++ 05-EjerciciosT5/Ejercicio11/CabreoException.java
@@ -0,0 +1,19 @@
+
+package tema5.ejercicio11;
+
+/**
+ *
+ * @author álvaro <alvarojimgil12@gmail.com>
+ */
+
+import java.lang.Exception;
+
+public class CabreoException extends Exception{
+    
+    public CabreoException(String m){
+        
+        
+        
+    }
+
+}
